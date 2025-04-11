dc95b4e588f1ddec7f3687d40397d4beff998f9f
Lautaro Di Sanza <lautarod@gmail.com>
2025-04-11 10:09:09 -0300

    Add security features

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
@@ -7,10 +8,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ["log", "fatal", "error", "warn"],
   });
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   const config = new DocumentBuilder()
     .setTitle('CrowdWisdom Relayer')
     .setDescription('CrowdWisdom Relayer Description')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
